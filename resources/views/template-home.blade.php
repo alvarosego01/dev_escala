55c0665a1bcd1ebcc780cd008f4349e86162d893
Alvaro Segovia <alvarosego01@gmail.com>
2021-06-08 20:42:00 -0400

    *

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -13,73 +13,76 @@
   <div class="sections">
 
     <section class="customSection elementor-element">
-        <div class="section-row">
+      <div class="section-row">
 
-                <section class="innerSectionElement row">
+        <section class="innerSectionElement row">
 
-                          <div class="containElements">
+          <div class="containElements">
 
-                                <h2 class="primaryTitle greenBlue"><span class="">Acelera el crecimiento de tu negocio</span></h2>
-
-
-                                <p class="primaryText grayText"><strong >escala</strong> te permite implementar embudos de venta: la estrategia probada para hacer crecer empresas de manera rentable, sostenible y escalable</p>
-
-                          </div>
-
-                </section>
-
-                <section class="innerSectionElement row">
-
-                          <div class="containElements col-sm-12 col-md-6 col-lg-4">
-                              <div class="elements">
-
-                                <div class="containerImage">
-                                  <img src="/escala-dev/wp-content/uploads/2021/04/beneficio_01.svg" class="attachment-large size-large" alt="" loading="lazy" width="1024" height="998">
-                                </div>
-
-                                <h2 class="secondaryTitle"><span class="greenBlue">Da a conocer</span> <br>tu negocio</h2>
-
-                              </div>
-                          </div>
-                          <div class="containElements col-sm-12 col-md-6 col-lg-4">
-                              <div class="elements">
-
-                                <div class="containerImage">
-                                  <img src="/escala-dev/wp-content/uploads/2021/04/beneficio_01.svg" class="attachment-large size-large" alt="" loading="lazy" width="1024" height="998">
-                                </div>
-
-                                <h2 class="secondaryTitle"><span class="greenBlue">Da a conocer</span> <br>tu negocio</h2>
-
-                              </div>
-                          </div>
-                          <div class="containElements col-sm-12 col-md-6 col-lg-4">
-                              <div class="elements">
-
-                                <div class="containerImage">
-                                  <img src="/escala-dev/wp-content/uploads/2021/04/beneficio_01.svg" class="attachment-large size-large" alt="" loading="lazy" width="1024" height="998">
-                                </div>
-
-                                <h2 class="secondaryTitle"><span class="greenBlue">Da a conocer</span> <br>tu negocio</h2>
-
-                              </div>
-                          </div>
-                          <div class="containElements col-sm-12 col-md-6 col-lg-4">
-                              <div class="elements">
-
-                                <div class="containerImage">
-                                  <img src="/escala-dev/wp-content/uploads/2021/04/beneficio_01.svg" class="attachment-large size-large" alt="" loading="lazy" width="1024" height="998">
-                                </div>
-
-                                <h2 class="secondaryTitle"><span class="greenBlue">Da a conocer</span> <br>tu negocio</h2>
-
-                              </div>
-                          </div>
+            <h2 class="primaryTitle greenBlue"><span class="">Acelera el crecimiento de tu negocio</span></h2>
 
 
+            <p class="primaryText grayText"><strong>escala</strong> te permite implementar embudos de venta: la estrategia probada para hacer crecer empresas de manera rentable, sostenible y escalable</p>
 
-                </section>
+          </div>
 
-        </div>
+        </section>
+
+        <section class="innerSectionElement row">
+
+          <div class="containElements col-sm-12 col-md-6 col-lg-3">
+            <div class="elements">
+
+              <div class="containerImage">
+                <img src="/escala-dev/wp-content/uploads/2021/04/beneficio_01.svg" class="attachment-large size-large" alt="" loading="lazy" width="1024" height="998">
+              </div>
+
+              <h2 class="secondaryTitle"><span class="greenBlue">Da a conocer</span> <br>tu negocio</h2>
+
+            </div>
+          </div>
+          <div class="containElements col-sm-12 col-md-6 col-lg-3">
+            <div class="elements">
+
+              <div class="containerImage">
+                <img src="/escala-dev/wp-content/uploads/2021/04/beneficio_02.svg" class="attachment-large size-large" alt="" loading="lazy" width="1024" height="998">
+              </div>
+
+
+              <h2 class="secondaryTitle"><span class="greenBlue">Multiplica</span> tus<br> clientes potenciales</h2>
+
+            </div>
+          </div>
+          <div class="containElements col-sm-12 col-md-6 col-lg-3">
+            <div class="elements">
+
+              <div class="containerImage">
+                <img src="/escala-dev/wp-content/uploads/2021/04/beneficio_03.svg" class="attachment-large size-large" alt="" loading="lazy" width="1024" height="998">
+              </div>
+
+
+              <h2 class="secondaryTitle"><span class="greenBlue">Aumenta </span><br>tus ventas</h2>
+
+            </div>
+          </div>
+          <div class="containElements col-sm-12 col-md-6 col-lg-3">
+            <div class="elements">
+
+              <div class="containerImage">
+                <img src="/escala-dev/wp-content/uploads/2021/04/beneficio_04.svg" class="attachment-large size-large" alt="" loading="lazy" width="1024" height="998">
+              </div>
+
+
+              <h2 class="secondaryTitle"><span class="greenBlue">Fideliza</span><br>a tus clientes</h2>
+
+            </div>
+          </div>
+
+
+
+        </section>
+
+      </div>
     </section>
 
   </div>
@@ -88,7 +91,8 @@
 
 
 
-{{--
+
+      {{--
   @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
     @include('partials.content-page')
@@ -96,4 +100,4 @@
   --}}
 
 
-@endsection
+      @endsection
