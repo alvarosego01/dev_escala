054759a65aaa014fce07e325b265c2ce455449c9
Alvaro Segovia <alvarosego01@gmail.com>
2021-06-20 08:16:08 -0400

    Ajustes

--- resources/views/template-func-crm-escala.blade.php
+++ resources/views/template-func-crm-escala.blade.php
@@ -2,3 +2,22 @@
 {{--
   Template Name: [B] Func - CRM escala
 --}}
+
+
+
+@extends('layouts.app')
+
+@section('content')
+
+<div id="func-crm-bootstrap">
+
+  <div class="sections">
+
+
+  </div>
+
+</div>
+
+
+
+@endsection
--- resources/views/template-func-digital-announces.blade.php
+++ resources/views/template-func-digital-announces.blade.php
@@ -2,3 +2,22 @@
 {{--
   Template Name: [B] Func - Anuncios digitales
 --}}
+
+
+
+@extends('layouts.app')
+
+@section('content')
+
+<div id="func-announceDigital-bootstrap">
+
+  <div class="sections">
+
+
+  </div>
+
+</div>
+
+
+
+@endsection
