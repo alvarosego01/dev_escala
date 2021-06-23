0065bb308d011e1d90c0c29192dbb5ccc7937479
Alvaro Segovia <alvarosego01@gmail.com>
2021-06-23 11:05:54 -0400

    Ajustes

--- resources/views/template-func-digital-announces.blade.php
+++ resources/views/template-func-digital-announces.blade.php
@@ -23,9 +23,11 @@
      'overlay' => true,
      'classSection' => 'threeCol funcAnnounceDigital0 func',
      'title' => '<span class="greenBlueColor">Atrae visitantes</span> <br> como un imán',
-     'text' => 'Crea, publica y administra tus <br> campañas de anuncios <br> digitales, desde una sola <br> plataforma.',
+     'text' => 'Crea, publica y administra tus <br> campañas de anuncios digitales, <br> desde una sola plataforma.',
      'image' => 'https://cdn.escala.com/wp-content/uploads/sites/2/2021/06/escaaads01.png',
-     'textForm' => '<span class="greenBlueColor">Impulsa</span> tus ventas ahora',
+     'textForm' => 'Empieza a probar escala
+  <br style="display: block!important">
+  <span style="text-align: left"; width: 100% class="colorDarkGrey2">Sin tarjeta requerida</span>',
      'threeCol' => true,
      'backgroundImage' => 'https://cdn.escala.com/wp-content/uploads/sites/2/2021/06/pagebuilder-bg.png',
       'overlayImage' => 'https://cdn.escala.com/wp-content/uploads/sites/2/2021/06/escalaads-planets.svg',
@@ -60,10 +62,10 @@
   'classSection' => 'funcAnnounceDigital2',
   'enableTitle' => true,
   'titlePrincipal' => '<span class="greenBlueColor">Genera tráfico a tus páginas</span> <br> sin que tu billetera sufra',
-  'subTitlePrincipal' => 'Crea y maneja potentes campañas digitales en <span class="orangeColor">escala</span>',
+  'subTitlePrincipal' => 'Crea y maneja potentes campañas digitales en <strong class="orangeColor">escala</strong>',
   'title' => '<span class="greenBlueColor">Segmenta y consigue</span> <br> más clientes potenciales',
   'img' => 'https://cdn.escala.com/wp-content/uploads/sites/2/2021/06/escaaads03-280x300.png',
-  'text' => 'Una de las claves para crear anuncios exitosos es escoger <br> correctamente el público al que deseas dirigir tus campañas de <br> mercadeo. En escala encuentras las herramientas que necesitas <br> para segmentar y aprovechar al máximo el poder de los medios <br> digitales.',
+  'text' => 'Una de las claves para crear anuncios exitosos es escoger <br> correctamente el público al que deseas dirigir tus campañas de <br> mercadeo. En escala encuentras las herramientas que necesitas para <br> segmentar y aprovechar al máximo el poder de los medios digitales.',
   'enableButton' => false,
   // 'urlButton' => '#lead-form',
   // 'textButton' => 'Segmentar ahora',
@@ -82,10 +84,8 @@
   'enableTitle' => false,
   'img' => 'https://cdn.escala.com/wp-content/uploads/sites/2/2021/06/escaaads04-230x300.png',
   'title' => '<span class="whiteColor">Diseña potentes anuncios digitales. <br> Atrae a tu audiencia ideal</span>',
-  'text' => 'Despierta tu creatividad creando anuncios de Facebook e Instagram fácilmente en escala.
-  <br class="space"><br class="space">
-Simplemente agrega textos que enganchen, escoge un titular irresistible, y selecciona la foto o el video que mejor comunique
-tu propuesta de valor.
+  'text' => 'Despierta tu creatividad creando anuncios de Facebook e Instagram <br> fácilmente en escala.
+<br class="space"><br class="space"> Simplemente agrega textos que enganchen, escoge un titular irresistible, <br> y selecciona la foto o el video que mejor comunique <br> tu propuesta de valor.
 <br class="space"><br class="space">
 ¡Todo es más fácil desde la plataforma!',
   'enableButton' => true,
@@ -108,7 +108,7 @@ tu propuesta de valor.
   'img' => 'https://cdn.escala.com/wp-content/uploads/sites/2/2021/06/escaaads05-300x232.png',
   'title' => '<span class="greenBlueColor">Conecta fácilmente</span> <br> tus anuncios digitales <br> a tus páginas',
   'text' => 'Una vez que has creado potentes campañas de anuncios digitales <br> en escala, simplemente selecciona el landing page al que deseas <br> dirigir el tráfico ¡y voilà!
-  <br class="space"><br class="space">
+<br class="space"><br class="space">
 Estás listo para desbordar tus landing pages con visitantes.',
   'enableButton' => false,
   // 'urlButton' => '#lead-form',
@@ -129,11 +129,13 @@ Estás listo para desbordar tus landing pages con visitantes.',
   'enableTitle' => false,
   'img' => 'https://cdn.escala.com/wp-content/uploads/sites/2/2021/06/escaaads06-292x300.png',
   'title' => '<span class="greenBlueColor">¿Listo para publicar?</span> <br> *Hazlo en un click*',
-  'text' => 'Con escala, publicar tus campañas en Facebook e Instagram es <br> fácil y rápido.
-  <br class="space"><br class="space">
+  'text' => 'Con escala, publicar tus campañas en Facebook <br>
+e Instagram es fácil y rápido.
+<br class="space"><br class="space">
 ¡Solo necesitas un click!
 <br class="space"><br class="space">
-Siéntate, relájate y observa cómo tus anuncios hacen su magia <br> para traer tráfico a tus páginas.',
+Siéntate, relájate y observa cómo tus anuncios <br>
+hacen su magia para traer tráfico a tus páginas.',
   'enableButton' => false,
   // 'urlButton' => '#lead-form',
   // 'textButton' => 'Publicar modo flash',
@@ -153,9 +155,11 @@ Siéntate, relájate y observa cómo tus anuncios hacen su magia <br> para traer
   'enableTitle' => false,
   'img' => 'https://cdn.escala.com/wp-content/uploads/sites/2/2021/06/escaaads07-281x300.png',
   'title' => '<span class="greenBlueColor">¡Optimizar,</span> <br> siempre optimizar!',
-  'text' => 'Monitorea en tiempo real el rendimiento de tus campañas de <br> anuncios digitales con escala ¿Cuántas personas han visto tu <br> anuncio? ¿Cuál es tu costo por click?
-  <br class="space"><br class="space">
-Lo que no se mide no mejora. Conoce lo que funciona, aprende de <br> lo que no y mejora con ello tus resultados.',
+  'text' => 'Monitorea en tiempo real el rendimiento de tus campañas <br> de anuncios digitales con escala. <br>
+¿Cuántas personas han visto tu anuncio? <br>
+¿Cuál es tu costo por click?
+<br class="space"><br class="space">
+Lo que no se mide no mejora. Conoce lo que funciona, <br> aprende de lo que no y mejora con ello tus resultados.',
   'enableButton' => true,
   'urlButton' => '#lead-form',
   'textButton' => 'Optimizar resultados',
@@ -179,13 +183,16 @@ Lo que no se mide no mejora. Conoce lo que funciona, aprende de <br> lo que no y
   'img' => 'https://cdn.escala.com/wp-content/uploads/sites/2/2021/06/pagebuilder14-265x300.png',
   'title' => '<span class="whiteColor">tus anuncios digitales</span> <br> ¿Qué es un Pixel y por qué <br> es importante?
   ',
-  'text' => '<strong>Crea y maneja potentes campañas digitales en <span class="orangeColor">escala</span></strong>
+  'text' => '
+  <span style="text-align: center; font-size: 20px; font-weight: 600!important">Crea y maneja potentes campañas digitales en <span class="orangeColor">escala</span></span>
   <br class="space"><br class="space">
-Imagina poder saber: qué campaña de anuncios digitales generó <br> más visitas a tu landing page, qué segmentación de audiencia <br> generó más contactos, qué anuncio generó más clientes, ¡y mucho <br> más!
+  Imagina poder saber: qué campaña de anuncios <br> digitales generó más visitas a tu landing page, qué <br> segmentación de audiencia generó más contactos, qué <br> anuncio generó más clientes, ¡y mucho más!
 <br class="space"><br class="space">
-Cuando tu landing page tiene el Pixel integrado, tú y Facebook <br> obtienen información que permite optimizar rápidamente tus <br> campañas de anuncios digitales.
+Cuando tu landing page tiene el Pixel integrado, tú y <br> Facebook obtienen información que permite optimizar <br> rápidamente tus campañas de anuncios digitales.
 <br class="space"><br class="space">
-Las páginas creadas en escala ya vienen con el Pixel de Facebook <br> integrado.',
+Las páginas creadas en escala ya vienen <br>
+con el Pixel de Facebook integrado.
+  ',
   'enableButton' => false,
   // 'urlButton' => '#lead-form',
   // 'textButton' => '¡Ve rápido y furioso!',
