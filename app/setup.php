4f3d82581249c5439eec6d56bfa296e7609a02cd
Alvaro Segovia <alvarosego01@gmail.com>
2021-06-26 12:48:42 -0400

    cf7 prueba

--- app/setup.php
+++ app/setup.php
@@ -12,17 +12,17 @@ use Roots\Sage\Template\BladeProvider;
 use App\Controllers\App;
 
 
-/**
- * Disables REFILL function in WPCF7 if Recaptcha is in use
- */
-add_action('wp_enqueue_scripts', 'wpcf7_recaptcha_no_refill', 15, 0);
-function wpcf7_recaptcha_no_refill() {
-  $service = WPCF7_RECAPTCHA::get_instance();
-	if ( ! $service->is_active() ) {
-		return;
-	}
-  wp_add_inline_script('contact-form-7', 'wpcf7.cached = 0;', 'before' );
-}
+// /**
+//  * Disables REFILL function in WPCF7 if Recaptcha is in use
+//  */
+// add_action('wp_enqueue_scripts', 'wpcf7_recaptcha_no_refill', 15, 0);
+// function wpcf7_recaptcha_no_refill() {
+//   $service = WPCF7_RECAPTCHA::get_instance();
+// 	if ( ! $service->is_active() ) {
+// 		return;
+// 	}
+//   wp_add_inline_script('contact-form-7', 'wpcf7.cached = 0;', 'before' );
+// }
 
 /**
  * Theme assets
