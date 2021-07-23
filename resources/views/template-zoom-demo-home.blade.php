e5acc34c79d30be1ecf7eaf96818972a89c2bcfc
Alvaro Segovia <alvarosego01@gmail.com>
2021-07-23 11:08:20 -0400

    Ajustes

--- resources/views/template-zoom-demo-home.blade.php
+++ resources/views/template-zoom-demo-home.blade.php
@@ -1,5 +1,5 @@
 {{--
-  Template Name: [B] Zoom Demo Home
+  Template Name: [B] Zoom Demo Home 2
 --}}
 
 <link crossorigin="anonymous" rel="stylesheet" id="google-fonts-1-css" href="https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CNunito+Sans%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CNunito%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;ver=5.5.5&amp;display=swap" type="text/css" media="all">
@@ -61,10 +61,19 @@
 
                   <div class="formatForm">
 
+                    @if (isset($_GET['zoom']) && $_GET['zoom'] == '2'  )
+
                     <h4 class="titleFormat blackcolor">
-                      Solicita tu demo
+                    Recibe un Demo personalizado
                     </h4>
 
+                    @else
+
+                    <h4 class="titleFormat blackcolor">
+                      Conversa con un Asesor
+                    </h4>
+
+                    @endif
                     {{-- {!! do_shortcode( '[contact-form-7 id="73" title="Contacto Paso 1"]' ); !!} --}}
 
                     @php
