5829330a1ad515de922162f185c1c4ce7fd07fca
Alvaro Segovia <alvarosego01@gmail.com>
2021-12-03 16:38:44 +0100

    *

--- resources/views/components/contain/pricing/component-pricing-price-card-T1.blade.php
+++ resources/views/components/contain/pricing/component-pricing-price-card-T1.blade.php
@@ -165,7 +165,7 @@ $items = [
                 'items' => [
                 'Usuarios de venta: <strong>2</strong>',
                 // 'Contactos en base de datos: <strong>1.000.000</strong>',
-                'Emails marketing al mes: <strong>10.000</strong>',
+                'Emails marketing al mes: <strong>20.000</strong>',
                 'Dominios propios: <strong>5</strong>',
                 'Cuentas de Facebook Ads Manager: <strong>3</strong>'
             ],
