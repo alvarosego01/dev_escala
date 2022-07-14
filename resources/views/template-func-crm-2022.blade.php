262fcd9c35e685cc051bf94506b82a3fabdeaca3
Alvaro Segovia <alvarosego01@gmail.com>
2022-07-14 09:28:07 -0400

    progreso landing y func crm

---
+++ resources/views/template-func-crm-2022.blade.php
@@ -0,0 +1,15 @@
+
+{{--
+  Template Name: [B] Func - CRM 2022
+--}}
+
+
+
+@extends('layouts.app')
+@section('content')
+
+
+@include('pages.functionalities.template-func-crm-2022')
+
+
+@endsection
