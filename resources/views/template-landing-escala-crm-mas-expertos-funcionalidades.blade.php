838f2c73ea36f71322baaace13cacd5cc20b111f
Alvaro Segovia <alvarosego01@gmail.com>
2022-09-09 12:48:22 -0400

    Ajustes de landing crm expertos funcionalidades

---
+++ resources/views/template-landing-escala-crm-mas-expertos-funcionalidades.blade.php
@@ -0,0 +1,13 @@
+
+{{--
+  Template Name: [B] Landing - CRM expertos funcionalidades
+--}}
+
+@extends('layouts.app')
+@section('content')
+
+
+@include('pages.landingPages.template-landing-escala-crm-mas-expertos-funcionalidades')
+
+
+@endsection
