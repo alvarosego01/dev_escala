77b52037b3a4dbaf2a408c15f2013529cccac081
Alvaro Segovia <alvarosego01@gmail.com>
2023-02-23 16:35:20 -0400

    prueba

--- resources/views/pages/subPages/template-corporative-resentation.blade.php
+++ resources/views/pages/subPages/template-corporative-resentation.blade.php
@@ -2,27 +2,37 @@
 @php
     $file = ACF_CUSTOM::_getField('url_presentation');
 
+    echo $file;
 @endphp
+{{--
+
+    <script>
+ let $file_url = "{{ $file }}";
+function download(url, filename) {
+  fetch(url)
+    .then(response => response.blob())
+    .then(blob => {
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = filename;
+      link.click();
+  })
+  .catch(console.error);
+}
+
+download( $file_url,"presentacion.pdf");
+
+window.location = ('{{$home}}');
+
+
+</script> --}}
 
 
 @if ($file != null)
-<object
-	data="{{$file}}"
-	type="application/pdf"
-	width="100%"
-	height="100%"
->
-	<iframe
-		src="{{$file}}"
-		width="100%"
-		height="100%"
-		style="border: none"
-	>
-		<p>
-			Your browser does not support PDFs.
-			<a href="{{$file}}">Download the PDF</a>
-		</p>
-	</iframe>
+<object style="min-height: 1500px" data="{{$file}}" type="application/pdf">
+    <iframe  src="https://docs.google.com/viewer?url=your_url_to_pdf&embedded=true"></iframe>
 </object>
+----
+<iframe src="http://docs.google.com/gview?url={{$file}}&embedded=true" style="width:100%; height:1500px;" frameborder="0"></iframe>
 
 @endif
