147fdd2a17e21720295ab7718b9eaea2c6c896a6
Alvaro Segovia <alvarosego01@gmail.com>
2023-02-28 12:59:20 -0400

    avances actualizacion corporativa

--- resources/views/pages/subPages/template-corporative-resentation.blade.php
+++ resources/views/pages/subPages/template-corporative-resentation.blade.php
@@ -3,31 +3,9 @@
     $file = ACF_CUSTOM::_getField('url_presentation');
 
 @endphp
-{{--
-
-    <script>
- let $file_url = "{{ $file }}";
-function download(url, filename) {
-  fetch(url)
-    .then(response => response.blob())
-    .then(blob => {
-      const link = document.createElement("a");
-      link.href = URL.createObjectURL(blob);
-      link.download = filename;
-      link.click();
-  })
-  .catch(console.error);
-}
-
-download( $file_url,"presentacion.pdf");
-
-window.location = ('{{$home}}');
 
 
-</script> --}}
-
-
-@if ($file != null)
+{{-- @if ($file != null)
 
 @php
     $apiKey = null;
@@ -63,4 +41,11 @@ window.location = ('{{$home}}');
 
  </script>
 
+@endif --}}
+
+
+@if ($file != null)
+<object style="min-height: 1500px" data="{{$file}}" type="application/pdf">
+    <iframe style="min-height: 1500px" src="https://docs.google.com/viewer?url={{$file}}&embedded=true"></iframe>
+</object>
 @endif
