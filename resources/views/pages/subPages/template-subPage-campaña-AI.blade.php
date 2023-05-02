de998e999caaf9dd8505747ef328c2802d79968b
Alvaro Segovia <alvarosego01@gmail.com>
2023-05-02 16:44:12 -0400

    *¨***

--- resources/views/pages/subPages/template-subPage-campaña-AI.blade.php
+++ resources/views/pages/subPages/template-subPage-campaña-AI.blade.php
@@ -464,7 +464,7 @@ La IA Consigue <span>imágenes</span>
                 [
                     'area' => 'Salud y bienestar',
                     'text' => '<strong>“Aumentamos 5X la tasa de recuperación</strong> de carrito de compra abandonado y 41% la tasa de apertura promedio en campañas de Email Marketing.”',
-                    'logo' => App::setFilePath('/assets/images/logos/logo_rg.png'),
+                    'logo' => App::setFilePath('/assets/images/logos/logo_rgg.png'),
                     'name' => 'Biaggio Correale, Director <br class="space"> de Operaciones',
                 ],
             ];
