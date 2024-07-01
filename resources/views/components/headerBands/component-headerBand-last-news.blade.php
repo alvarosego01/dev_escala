6771f70b01a9c6bbc586d2e05637b543ce5ad731
Wikedhart18 <enderbrache18@gmail.com>
2024-07-01 13:23:49 -0400

    *Resolviendo problemas con el banner mobile

--- resources/views/components/headerBands/component-headerBand-last-news.blade.php
+++ resources/views/components/headerBands/component-headerBand-last-news.blade.php
@@ -29,5 +29,38 @@
         </section>
 
     </div>
+    </section>
 
-</section>
+
+<section style="background-image: url({!! App::setFilePath('/assets/images/banners/bg_automatizaciones_mobile.svg;') !!})" class="headerband_promo_lastNews customSection sectionParent fullWidth MbHeadband {{ $classSection }} ">
+
+    <div class="section-row">
+
+        <section class="innerSectionElement sct1">
+
+            <div class="containElements">
+
+                <div class="contain">
+
+                    {{-- <img src="{!! App::setFilePath('/assets/images/illustrations/others/icon_headerband_freeclass.png') !!}" alt="" class="icon"> --}}
+
+                    <span class="hashTitle">
+                    ¡Potencia tus automatizaciones!
+                    </span>
+                    <div class="separator"></div>
+                    <span class="sub">
+                    Descubre cómo podemos ayudarte a escalar tus  <br class="space"> comunicaciones con las nuevas funcionalidades.
+                    </span>
+
+                </div>
+
+                <a  class=" primaryButton hoverInEffect openPopUpButton popup-last-news">
+                    Explora más
+                </a>
+
+            </div>
+
+        </section>
+
+    </div>
+    </section>
