d89258a5733acd95ddea451a510c3a5424f1fee0
Wikedhart18 <enderbrache18@gmail.com>
2024-10-10 17:06:58 -0400

    Test blocks

---
+++ resources/views/components/blocks/blog/block-text-parag-2/block-text-parag-2.php
@@ -0,0 +1,14 @@
+<?php
+// Block Name: Blog Text Parag Block 2
+
+use App\Classes\Twig_Config;
+
+require_once THEME_ROOT_PATH . '/vendor/autoload.php';
+
+$twig = Twig_Config::getInstance()->getTwig();
+
+$slug = $context['slug'];
+$path = $context['path'];
+
+// echo $twig->render($path . "/{$slug}.twig", $context);
+echo "Holaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
