9f0a45aaeb7eaa097442cd8e0e15afbe49bbce6d
Wikedhart18 <enderbrache18@gmail.com>
2024-10-23 17:00:52 -0400

    Haciendo cambios en el Home

--- resources/views/template-home-2023.blade.php
+++ resources/views/template-home-2023.blade.php
@@ -938,8 +938,6 @@
                     </div>
                 </section>
 
-                <!-- Scripts de Bootstrap y jQuery -->
-                <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
             </div>
