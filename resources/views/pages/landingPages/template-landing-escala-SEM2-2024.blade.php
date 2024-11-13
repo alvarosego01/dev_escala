97d766d712751298e3ddbf283bd6b2efa2ee198e
Wikedhart18 <enderbrache18@gmail.com>
2024-11-13 13:44:58 -0400

    Aplicando el script a todos los formularios de escala

--- resources/views/pages/landingPages/template-landing-escala-SEM2-2024.blade.php
+++ resources/views/pages/landingPages/template-landing-escala-SEM2-2024.blade.php
@@ -4341,44 +4341,7 @@
 
 
                                 </div>
-                                <script>
-                                    jQuery(document).ready(function($) {
-                                        $(".containElements").each(function() {
-                                            var $currentContainer = $(this);
-
-                                            // Variables para almacenar los datos del primer formulario
-                                            var firstName = "",
-                                                lastName = "",
-                                                email = "";
-
-                                            // Capturar datos del primer formulario antes del envío
-                                            $currentContainer.find(".form-1").on('submit', function() {
-                                                firstName = $(this).find('input[name="your-firstname"]').val();
-                                                lastName = $(this).find('input[name="your-lastname"]').val();
-                                                email = $(this).find('input[name="your-email"]').val();
-                                            });
-
-                                            // Listener para el envío exitoso del formulario vía CF7
-                                            $(document).on('wpcf7mailsent', function(event) {
-                                                // Comprobar si el formulario enviado es el del contenedor actual
-                                                var $form = $(event.target);
-                                                var $formContainer = $form.closest('.containElements');
-
-                                                // Verifica si es el mismo contenedor
-                                                if ($formContainer.is($currentContainer)) {
-                                                    // Ocultar el primer formulario y mostrar el segundo
-                                                    $formContainer.find(".form-1").hide();
-                                                    $formContainer.find(".form-2").show();
-
-                                                    // Asignar valores capturados a los campos ocultos del segundo formulario
-                                                    $formContainer.find('#hidden-name-field').val(firstName);
-                                                    $formContainer.find('#hidden-lastname-field').val(lastName);
-                                                    $formContainer.find('#hidden-email-field').val(email);
-                                                }
-                                            });
-                                        });
-                                    });
-                                </script>
+                              
 
 
                             </div>
