9ad7ff86ffe3afe8db7680788957af752ae17392
Wikedhart18 <enderbrache18@gmail.com>
2024-12-21 07:48:20 -0400

    Pagina inicio/fin casos de exito Motored (revision)

--- app/Controllers/SetComponents.php
+++ app/Controllers/SetComponents.php
@@ -353,6 +353,7 @@ class SetComponents extends Controller
                 'views/template-casoExito-taller-5.blade.php',
                 'views/template-casoExito-desayunos-del-cielo.blade.php',
                 'views/template-casoExito-real-de-los-cues.blade.php',
+                'views/template-casoExito-motored.php',
                 'views/template-subPage-pricingPro.blade.php',
                 'views/template-subPage-pricing-2023.blade.php',
                 'views/template-corporative-resentation.blade.php',
