c65d322723da8192ca3505e63c0bd3afba5707ba
Wikedhart18 <enderbrache18@gmail.com>
2025-01-20 17:08:41 -0400

    Terminando banner mas otros combios pequeños

--- resources/views/components/headerBands/component-headerBand-free-11.blade.php
+++ resources/views/components/headerBands/component-headerBand-free-11.blade.php
@@ -8,20 +8,16 @@
 
                 <div class="contain">
 
-                    <img src="{!! App::setFilePath('/assets/images/illustrations/others/icon_masterclass_enero_2025.png') !!}" alt="" class="icon">
+                    <img src="{!! App::setFilePath('/assets/images/illustrations/others/icon_masterclass_enero_2025_2.png') !!}" alt="" class="icon">
 
                     <span class="hashTitle">
-                        <span>Crea procesos comerciales ganadores </span>
+                        <span>Optimiza procesos de ventas ganadores con CRM</span>
 
                     </span>
-
+                    <div class="separator"></div>
                     <span class="sub">
-                        - Masterclass gratuita:
+                        Masterclass gratuita - 22 Enero
                     </span>
-                    <span class="bg_orange">
-                        enero 22
-                    </span>
-
                 </div>
 
                 <a target="_blank" href="https://experiencia.escala.com/eventos-escala" class=" primaryButton hoverInEffect ">
@@ -36,38 +32,33 @@
 </section>
 
 
-<section style="background-image: url({!! App::setFilePath('/assets/images/banners/free_bg_mb_MC_09_01.svg') !!})" class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth MbHeadbandfree {{ $classSection }} ">
+<section style="background-image: url({!! App::setFilePath('/assets/images/banners/headerband_freeclass_enero_2025_mb.svg') !!})" class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth MbHeadbandfree {{ $classSection }} ">
 
     <div class="section-row">
 
         <section class="innerSectionElement sct1">
 
+            <div class="containElements">
 
-            <div class="contain">
+                <div class="contain">
 
-                <img src="{!! App::setFilePath('/assets/images/illustrations/others/icon_masterclass_enero_2025.png') !!}" alt="" class="icon">
+                    <span class="hashTitle">
+                        <span>Optimiza procesos de ventas ganadores con CRM </span>
 
-                <span class="hashTitle">
-                    <span>Crea procesos comerciales ganadores </span>
+                    </span>
+              
+                    <span class="sub">
+                        Masterclass gratuita - 22 Enero
+                    </span>
+                </div>
 
-                </span>
-
-                <span class="sub">
-                    - Masterclass gratuita:
-                </span>
-                <span class="bg_orange">
-                    enero 22
-                </span>
+                <a target="_blank" href="https://experiencia.escala.com/eventos-escala" class=" primaryButton hoverInEffect ">
+                    Reserva tu cupo
+                </a>
 
             </div>
 
-            <a target="_blank" href="https://experiencia.escala.com/eventos-escala" class=" primaryButton hoverInEffect ">
-                Reserva tu cupo
-            </a>
+        </section>
 
     </div>
-
-</section>
-
-</div>
 </section>
