db045b763f273970f2cb459be42588a2d7d12abc
Wikedhart18 <enderbrache18@gmail.com>
2025-01-21 10:45:19 -0400

    Ajuste de horarios

--- resources/views/pages/subPages/template-subPage-zoom-meeting-2023.blade.php
+++ resources/views/pages/subPages/template-subPage-zoom-meeting-2023.blade.php
@@ -52,36 +52,19 @@
                                         </h3>
                                         <div class="text">
                                             <div>
-                                                <p>6:00 am - 7:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_mex_2.png') !!}" loading="lazy"> <br class="space">
+                                                <p>6:00 am - 6:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_mex_2.png') !!}" loading="lazy"> <br class="space">
                                             </div>
                                             <div>
                                                 <p>7:00 am - 7:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_col_per_usa_cr.png') !!}" loading="lazy"><br class="space">
                                             </div>
                                             <div>
-                                                <p>8:00 am - 9:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_ven_2.png') !!}" loading="lazy"> <br class="space">
+                                                <p>8:00 am - 8:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_ven_2.png') !!}" loading="lazy"> <br class="space">
                                             </div>
                                             <div>
-                                                <p>9:00 am - 10:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/banderas_arg_uru_chil_2.png') !!}" loading="lazy"><br class="space">
+                                                <p>9:00 am - 9:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/banderas_arg_uru_chil_2.png') !!}" loading="lazy"><br class="space">
                                             </div>
                                         </div>
-                                        <hr>
-                                        <h3>
-                                            Sábados:
-                                        </h3>
-                                        <div class="text">
-                                            <div>
-                                                <p> 8:00 am - 12:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_mex_2.png') !!}" loading="lazy"> <br class="space">
-                                            </div>
-                                            <div>
-                                                <p> 9:00 am - 1:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_col_per_usa_cr.png') !!}" loading="lazy"><br class="space">
-                                            </div>
-                                            <div>
-                                                <p> 10:00 am - 2:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_ven_2.png') !!}" loading="lazy"> <br class="space">
-                                            </div>
-                                            <div>
-                                                <p> 11:00 am - 3:00 pm. </p><img src="{!! App::setFilePath('/assets/images/illustrations/others/banderas_arg_uru_chil_2.png') !!}" loading="lazy"><br class="space">
-                                            </div>
-                                        </div>
+                                        
                                     </div>
                                 </div>
 
