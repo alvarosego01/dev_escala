c3ca3f262cb8f160b6d8a85b8c0654256ddd484a
Wikedhart18 <enderbrache18@gmail.com>
2025-02-27 14:18:06 -0400

    Cambio copi pagina pricing

--- resources/views/pages/subPages/template-subPage-pricing-2023.blade.php
+++ resources/views/pages/subPages/template-subPage-pricing-2023.blade.php
@@ -1388,11 +1388,18 @@ y conexión Meta Business ",
                                         <div class="containElements">
 
                                             <p class="text">
-                                                Para las cuentas mayores a USD 1.000, un Gerente de <br class="DT_e">
-                                                Éxito te apoya a definir un plan estratégico de <br class="DT_e">
-                                                marketing y/o ventas con Escala para luego guiarte <br class="DT_e">
-                                                a implementarlo paso a paso.
-
+                                                El servicio de Acompañamiento e Implementación <br class="DT_e">
+                                                Guiado de Escala, con un Gerente de Éxito <br class="DT_e">
+                                                dedicado, está disponible exclusivamente para <br class="DT_e">
+                                                cuentas Pro con planes anuales superiores a USD <br class="DT_e">
+                                                1,000 al año.
+                                                <br class="space">
+                                                <br class="space">
+                                                Las cuentas con un valor anual menor cuentan con <br class="DT_e">
+                                                soporte técnico ilimitado vía chat para resolver <br class="DT_e">
+                                                consultas y acceso completo a los materiales de <br class="DT_e">
+                                                Escala Academy, donde podrán aprender a <br class="DT_e">
+                                                configurar y utilizar todas las herramientas.
                                             </p>
 
                                         </div>
