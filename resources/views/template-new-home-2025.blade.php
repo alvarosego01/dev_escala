acb8d124592ee6415fff8c567e6e560bf6e1fd06
Ender Brache <enderbrache18@gmail.com>
2025-03-17 14:04:44 -0400

    Mas cambios home rebrand

--- resources/views/template-new-home-2025.blade.php
+++ resources/views/template-new-home-2025.blade.php
@@ -147,7 +147,84 @@
 
         </section>
 
+        <section class="component-header-t1 bg-image overlay customSection sectionParent fullWidth threeCol new-home-2025-0-1 ">
+            <div>
+                <div class="section-row">
+                    <section class="innerSectionElement sct2">
+                        <div class="groupElements row">
+                            <div class="info col-md-12 col-lg-8 ">
+                                <div class="containElements">
+                                    @php
+                                    $elementsReviews = [
+                                    [
+                                    'logo' => App::setFilePath('/assets/images/illustrations/others/google_tag.png'),
+                                    'text' => 'Escala / plataforma CRM',
+                                    'points' => '4.9 / 5',
+                                    ],
+                                    [
+                                    'logo' => App::setFilePath('/assets/images/illustrations/others/capterra_tag.png'),
+                                    'text' => 'Escala / plataforma CRM',
+                                    'points' => '4.8 / 5',
+                                    ],
+                                    [
+                                    'logo' =>
+                                    App::setFilePath('/assets/images/illustrations/others/trustpilot_img.png'),
+                                    'text' => 'Escala / plataforma CRM',
+                                    'points' => '4.8 / 5',
+                                    ]
+                                    ];
+                                    @endphp
+                                    <div class="ele reviews">
+                                        <div class="elements">
+                                            <div class="iconApp">
+                                                <a target="_blank"
+                                                    href="https://www.getapp.com/customer-management-software/crm/category-leaders">
+                                                    <img src="{!! App::setFilePath('/assets/images/illustrations/others/img_app_record_2025_category.svg') !!}"
+                                                        loading="lazy">
+                                                </a>
+                                            </div>
+                                            @foreach ($elementsReviews as $item)
+                                            <div class="refersElement">
 
+                                                <div class="infoInner">
+                                                    <div class="tag">
+                                                        <div class="containerImage">
+                                                            <img src="{!! $item['logo'] !!}" loading="lazy">
+                                                        </div>
+
+                                                        <span class="points">
+                                                            {!! $item['points'] !!}
+                                                        </span>
+                                                    </div>
+                                                    <p class="text">
+                                                        {!! $item['text'] !!}
+                                                    </p>
+                                                    <div class="stars">
+                                                        <div class="containerImage">
+                                                            <img src="{!! App::setFilePath('/assets/images/illustrations/others/icons-stars-yellow.svg') !!}"
+                                                                loading="lazy">
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+
+                                            </div>
+                                            @endforeach
+
+                                        </div>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                </div>
+
+            </div>
+
+        </section>
         <section class="customSection sectionParent new-home-2025-1">
 
             <div class="section-row">
