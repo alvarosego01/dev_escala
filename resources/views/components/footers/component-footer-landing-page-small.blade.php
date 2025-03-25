50746f6ab0469d8275046f0d76360431cf37063d
Ender Brache <enderbrache18@gmail.com>
2025-03-25 13:42:02 -0400

    Nueva presentacion zoom meeting

--- resources/views/components/footers/component-footer-landing-page-small.blade.php
+++ resources/views/components/footers/component-footer-landing-page-small.blade.php
@@ -1,20 +1,20 @@
+<footer id="bootstrap_landing_small" class="content-info custom_PiePagina bootstrap_landing_small landing_small">
+
+    <div class="section-row">
 
 
-    <footer id="bootstrap_landing_small" class="content-info custom_PiePagina bootstrap_landing_small landing_small">
-
-        <div class="section-row">
-
-
-            <div class="containElements">
-                <p class="text">
-                    ©2024 All Rights Reserved. <br class="space">
-                    Cookie Preferences, Privacy, and Terms.
-                </p>
-            </div>
-
-
+        <div class="containElements">
+            <p class="text">
+                2022 © ExitoWeb, Inc. Todos los derechos reservados. Términos de servicio
+                <a target="_blank" href="https://escala.com/terminos-de-servicio/">(ES) </a>
+                <a target="_blank" href="https://escala.com/terms-of-service/">(EN)</a>
+                | Política de privacidad <a target="_blank" href="https://escala.com/politica-de-privacidad/">(ES)</a>
+                <a target="_blank" href="https://escala.com/privacy-policy/">(EN)</a> |
+                <a target="_blank" href="https://escala.com/vulnerability-program/">Report Vulnerability </a>
+            </p>
         </div>
 
-    </footer>
 
+    </div>
 
+</footer>
