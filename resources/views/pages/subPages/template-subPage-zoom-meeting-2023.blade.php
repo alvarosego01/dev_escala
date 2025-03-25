50c786f5f4ed2192fd1d219b285f33ca3941502d
Ender Brache <enderbrache18@gmail.com>
2025-03-25 14:07:25 -0400

    Cambios banderas zoom meeting

--- resources/views/pages/subPages/template-subPage-zoom-meeting-2023.blade.php
+++ resources/views/pages/subPages/template-subPage-zoom-meeting-2023.blade.php
@@ -64,10 +64,10 @@
                                                 <p>6:00 am - 6:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_mex_2.png') !!}" loading="lazy"> <br class="space">
                                             </div>
                                             <div>
-                                                <p>7:00 am - 7:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_col_per_usa_cr.png') !!}" loading="lazy"><br class="space">
+                                                <p>7:00 am - 7:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_peru_col_r.png') !!}" loading="lazy"><br class="space">
                                             </div>
                                             <div>
-                                                <p>8:00 am - 8:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_ven_2.png') !!}" loading="lazy"> <br class="space">
+                                                <p>8:00 am - 8:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/bandera_vez_usa.png') !!}" loading="lazy"> <br class="space">
                                             </div>
                                             <div>
                                                 <p>9:00 am - 9:00 pm.</p> <img src="{!! App::setFilePath('/assets/images/illustrations/others/banderas_arg_uru_chil_2.png') !!}" loading="lazy"><br class="space">
