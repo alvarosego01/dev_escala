fdf9fa5e3d01c77187fd065783e68c240cfc9f47
Ender Brache <enderbrache18@gmail.com>
2025-04-04 14:25:41 -0400

    Merge branch 'ramaEnderalternativa' into production

--- resources/views/components/headerBands/component-headerBand-free-11.blade.php
+++ resources/views/components/headerBands/component-headerBand-free-11.blade.php
@@ -1,4 +1,4 @@
-<section style="background-image: url({!! App::setFilePath('/assets/images/banners/headerband_freeclass_marzo_2025.svg') !!})" class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth {{ $classSection }} ">
+<section style="background-image: url({!! App::setFilePath('/assets/images/banners/bg_banner_mc_ai_abril_2025_white.svg') !!})" class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth {{ $classSection }} ">
 
     <div class="section-row">
 
@@ -8,20 +8,20 @@
 
                 <div class="contain">
 
-                    <img src="{!! App::setFilePath('/assets/images/illustrations/others/img_headerband_masterclass_2025_marzo_escala.png') !!}" alt="" class="icon">
+                    <img src="{!! App::setFilePath('/assets/images/illustrations/others/icon_ai_banner_mc_2025.png') !!}" alt="" class="icon">
 
                     <span class="hashTitle">
-                        <span><strong>Vende más con</strong> Whatsapp automatizado + CRM </span>
+                        <span><strong>Aumenta las inscripciones </strong> de tu empresa educativa con <span class="colorSpecial">Inteligencia Artificial</span></span>
 
                     </span>
                     <div class="separator"></div>
                     <span class="sub">
-                    Masterclass gratuita - 19 de Marzo
+                        Masterclass gratuita - 9 de Abril
                     </span>
                 </div>
 
                 <a target="_blank" href="https://experiencia.escala.com/eventos-escala" class=" primaryButton hoverInEffect ">
-                    Reserva tu cupo
+                    Reserva tu cupo →
                 </a>
 
             </div>
@@ -32,30 +32,30 @@
 </section>
 
 
-<section style="background-image: url({!! App::setFilePath('/assets/images/banners/headerband_freeclass_marzo_2025_mb.svg') !!})" class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth MbHeadbandfree {{ $classSection }} ">
+<section style="background-image: url({!! App::setFilePath('/assets/images/banners/bg_banner_mc_ai_abril_2025_mb_white.svg') !!})" class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth MbHeadbandfree {{ $classSection }} ">
 
     <div class="section-row">
 
         <section class="innerSectionElement sct1">
 
             <div class="containElements">
-
                 <div class="contain">
 
+                    <img src="{!! App::setFilePath('/assets/images/illustrations/others/icon_ai_banner_mc_2025.png') !!}" alt="" class="icon">
+
                     <span class="hashTitle">
-                        <span><strong>Vende más con</strong> Whatsapp automatizado + CRM </span>
+                        <span><strong>Aumenta las inscripciones </strong> de tu empresa educativa con <span class="colorSpecial">Inteligencia Artificial</span></span>
 
                     </span>
-              
+                    <div class="separator"></div>
                     <span class="sub">
-                    Masterclass gratuita - 19 de Marzo
+                        Masterclass gratuita - 9 de Abril
                     </span>
                 </div>
 
                 <a target="_blank" href="https://experiencia.escala.com/eventos-escala" class=" primaryButton hoverInEffect ">
-                    Reserva tu cupo
+                    Reserva tu cupo →
                 </a>
-
             </div>
 
         </section>
