43f0bc9d33824e2c3f37bed987fb2314126b1ded
Ender Brache <enderbrache18@gmail.com>
2025-05-13 13:20:32 -0400

    Ajustando pagina omnicanal imagenes slider

--- resources/views/pages/subPages/template-subPage-omnicanal-2025.blade.php
+++ resources/views/pages/subPages/template-subPage-omnicanal-2025.blade.php
@@ -261,15 +261,15 @@
                 <section class="innerSectionElement sct1">
                     <div class="mobile-contain-slider">
 
-                        <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-1-mb.png') !!}"
+                        <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-1-1-mb.png') !!}"
                             alt="Imagen 1 Desktop" class="mobile-img">
 
 
-                        <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-2-mb.png') !!}"
+                        <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-2-2-mb.png') !!}"
                             alt="Imagen 2 Desktop" class="mobile-img">
 
 
-                        <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-3-mb.png') !!}"
+                        <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-3-3-mb.png') !!}"
                             alt="Imagen 3 Desktop" class="mobile-img">
 
                     </div>
@@ -277,17 +277,17 @@
                     <div class="slider-container" id="sliderContainer">
                         <div class="slider" id="slider">
                             <div class="slide">
-                                <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-1.png') !!}"
+                                <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-1-1.png') !!}"
                                     alt="Imagen 1 Desktop" class="desktop-img">
 
                             </div>
                             <div class="slide">
-                                <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-2.png') !!}"
+                                <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-2-2.png') !!}"
                                     alt="Imagen 2 Desktop" class="desktop-img">
 
                             </div>
                             <div class="slide">
-                                <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-3.png') !!}"
+                                <img src="{!! App::setFilePath('/assets/images/illustrations/others/omnicanal-slider-3-3.png') !!}"
                                     alt="Imagen 3 Desktop" class="desktop-img">
 
                             </div>
