70975a91552a90fdc75bf2d2c2ca171b0608aa04
Ender Brache <enderbrache18@gmail.com>
2025-06-25 21:28:20 -0400

    Anuncios digitales continuacion mobile

--- resources/views/pages/subPages/platform/template-subPage-anuncios-2023.blade.php
+++ resources/views/pages/subPages/platform/template-subPage-anuncios-2023.blade.php
@@ -15,7 +15,7 @@
                                 digitales, desde una sola plataforma.
                             </p>
                             <div class="containerImage">
-                                <img alt="Ilustración de CEO ESCALA" src="{!! App::setFilePath('/assets/images/illustrations/others/ceo-escala-2025-alfonso-hero-anuncios.webp') !!}" loading="lazy">
+                                <img alt="Ilustración de CEO ESCALA" src="{!! App::setFilePath('/assets/images/illustrations/others/ceo-escala-2025-alfonso-hero-anuncios-1.webp') !!}" loading="lazy">
                             </div>
                         </div>
                     </section>
@@ -131,6 +131,236 @@
             </div>
 
         </section>
+
+
+        <section class="customSection sectionParent anuncios_TB_2023_2">
+
+            <div class="section-row ">
+                <section class="innerSectionElement sct1" style="background-image: url('{{ App::setFilePath('/assets/images/banners/bg-anuncios-2025.webp') }}')">
+                    <div class="cards left">
+                        <p>“La manera más rápida de escalar tu negocio es <br class="DT_e">
+                            dominando el mercadeo digital. Cualquiera puede <br class="DT_e">
+                            publicar un anuncio digital, la clave está en saber <br class="DT_e">
+                            generar tráfico calificado a tu landing page al menor <br class="DT_e">
+                            costo posible. En Escala te brindamos todas las <br class="DT_e">
+                            herramientas para hacerlo posible”.</p>
+
+                        <span class="colorEspecial">
+                            <b>Andrés Moreno,</b> <br class="space">
+                            Fundador de escala & Open English
+                        </span>
+                    </div>
+                    <div class="cards right ">
+                        <img src="{!! App::setFilePath('/assets/images/illustrations/others/img-am-escala-automata-2025.webp') !!}"
+                            alt="">
+                    </div>
+                </section>
+
+            </div>
+        </section>
+
+
+
+        <section class="w-full customSection sectionParent anuncios_TB_2023_3">
+            <div class="section-row">
+                <section class="innerSectionElement sct0 ">
+                    <div class="containElements">
+
+                        <h2 class="primaryTitle">
+                            Genera tráfico de clientes <br class="space">
+                            potenciales a tus páginas
+                        </h2>
+
+                    </div>
+                </section>
+
+                <section class="innerSectionElement sct1 ">
+                    <div class="containElements">
+
+                        @php
+                        $parameters = [
+                        'type' => 'backgroundColor',
+                        'classSection' => 'anuncios_TB_2023_4',
+                        'img' => App::setFilePath('/assets/images/illustrations/others/segmenta-mas-clientes-anuncios.webp'),
+                        'title' => 'Segmenta y consigue <br class="space">
+                        más clientes potenciales
+                        ',
+                        'text' => 'Una de las claves para crear anuncios exitosos es <br class="DT_e">
+                        escoger correctamente el público al que deseas <br class="DT_e">
+                        dirigir tus campañas de mercadeo. En Escala <br class="DT_e">
+                        encuentras las herramientas que necesitas para <br class="DT_e">
+                        segmentar y aprovechar al máximo el poder de los medios digitales.',
+                        'side' => 'right',
+                        ];
+                        @endphp
+                        @contain_text_image_T1($parameters)
+                        @endcontain_text_image_T1
+
+                        @php
+                        $parameters = [
+                        'type' => 'backgroundColor',
+                        'classSection' => 'anuncios_TB_2023_4_1',
+                        'img' => App::setFilePath('/assets/images/illustrations/others/diseño-potentes-anuncios-.webp'),
+                        'title' => 'Diseña potentes <br class="space">
+                        anuncios digitales. <br class="space">
+                        Atrae a tu audiencia ideal',
+                        'text' => 'Despierta tu creatividad creando anuncios de <br class="DT_e">
+                        Facebook e Instagram fácilmente en Escala. <br class="space">
+                        <br class="space">
+                        Simplemente agrega textos que enganchen, escoge <br class="DT_e">
+                        un titular irresistible, y selecciona la foto o el video <br class="DT_e">
+                        que mejor comunique
+                        tu propuesta de valor.
+                        <span>¡Todo es más fácil desde la plataforma!</span>',
+                        'side' => 'left',
+                        ];
+                        @endphp
+                        @contain_text_image_T1($parameters)
+                        @endcontain_text_image_T1
+
+                        @php
+                        $parameters = [
+                        'type' => 'backgroundColor',
+                        'classSection' => 'anuncios_TB_2023_4_2',
+                        'img' => App::setFilePath('/assets/images/illustrations/others/conexion-de-anuncios-digitales.webp'),
+                        'title' => 'Conecta fácilmente <br class="space">
+                        tus anuncios digitales <br class="space">
+                        a tus páginas',
+                        'text' => 'Una vez que has creado potentes campañas de <br class="DT_e">
+                        anuncios digitales en escala, simplemente <br class="DT_e">
+                        selecciona el landing page al que deseas dirigir el <br class="DT_e">
+                        tráfico ¡y voilà!
+                        <br class="space">
+                        <br class="space">
+                        Estás listo para desbordar tus landing pages con <br class="DT_e">
+                        visitantes.',
+                        'side' => 'right',
+                        ];
+                        @endphp
+                        @contain_text_image_T1($parameters)
+                        @endcontain_text_image_T1
+
+                        @php
+                        $parameters = [
+                        'type' => 'backgroundColor',
+                        'classSection' => 'anuncios_TB_2023_4_3',
+                        'img' => App::setFilePath('/assets/images/illustrations/others/publicar-anuncios-con-un-clic.webp'),
+                        'title' => '¿Listo para publicar? <br class="space">
+                        *Hazlo en un click*',
+                        'text' => 'Con Escala, publicar tus campañas en Facebook <br class="DT_e">
+                        e Instagram es fácil y rápido.
+                        <br class="space"><br class="space">
+                        ¡Solo necesitas un click!
+                        <br class="space"><br class="space">
+                        Siéntate, relájate y observa cómo tus anuncios <br class="DT_e">
+                        hacen su magia para traer tráfico a tus páginas.',
+                        'side' => 'left',
+                        ];
+                        @endphp
+                        @contain_text_image_T1($parameters)
+                        @endcontain_text_image_T1
+
+                        @php
+                        $parameters = [
+                        'type' => 'backgroundColor',
+                        'classSection' => 'anuncios_TB_2023_4_4',
+                        'img' => App::setFilePath('/assets/images/illustrations/others/optimizar-anuncios.webp'),
+                        'title' => '¡Optimizar, siempre optimizar!',
+                        'text' => 'Monitorea en tiempo real el rendimiento de tus <br class="DT_e">
+                        campañas de anuncios digitales con escala. <br class="DT_e">
+                        ¿Cuántas personas han visto tu anuncio? ¿Cuál es tu
+                        costo por click?
+                        <br class="space"><br class="space">
+                        Lo que no se mide no mejora. Conoce lo que <br class="DT_e">
+                        funciona, aprende de lo que no y mejora con ello tus
+                        resultados.',
+                        'side' => 'right',
+                        ];
+                        @endphp
+                        @contain_text_image_T1($parameters)
+                        @endcontain_text_image_T1
+
+
+                    </div>
+                </section>
+
+            </div>
+            <div class="btnCenter">
+                <a class="primaryButton hoverInEffect openPopUpButton popup-general-demo-2022">
+                    Optimizar resultados →
+                </a>
+            </div>
+        </section>
+
+
+        <section class="customSection sectionParent anuncios_TB_2023_5">
+
+            <div class="section-row ">
+                <section class="innerSectionElement sct1">
+                    <div class="containElements">
+                        <h2 class="primaryTitle">
+                            <span>Tus anuncios digitales</span>
+                            ¿Qué es un Pixel y por qué es importante?
+                        </h2>
+                        <p>Imagina poder saber: qué campaña de anuncios digitales generó más visitas a tu <br class="DT_e">
+                            landing page, qué segmentación de audiencia generó más contactos, qué anuncio <br class="DT_e">
+                            generó más clientes, ¡y mucho más!</p>
+                        <p>Cuando tu landing page tiene el Pixel integrado, tú y Facebook obtienen información <br class="DT_e">
+                            que permite optimizar rápidamente tus campañas de anuncios digitales.</p>
+                        <p>Las páginas creadas en Escala ya vienen <br class="DT_e">
+                            con el Pixel de Facebook integrado.</p>
+                    </div>
+                </section>
+
+            </div>
+        </section>
+
+
+
+
+
+        <section class="customSection sectionParent anuncios_TB_2023_6">
+            <div class="section-row " style="background-image: url('{{ App::setFilePath('/assets/images/banners/bg-anuncios-2025-section-6.webp') }}')">
+                <div class="containElements">
+                    <section class="innerSectionElement sct1">
+                        <div class="containElement">
+                            <h2 class="title">
+                                ¡Consigue más visitantes <br class="space">
+                                calificados ahora!
+                            </h2>
+
+                            <a class="primaryButton hoverInEffect openPopUpButton popup-general-demo-2022">
+                                Probar email marketing ahora →
+                            </a>
+
+                        </div>
+                    </section>
+
+
+
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     </div>
 </div>
