324e997040891043980ca636809425ebe269c9d1
Ender Brache <enderbrache18@gmail.com>
2025-07-03 11:37:31 -0400

    Nuevo cambio logo escala

--- resources/views/components/navBar/component-navbar-default-extended.blade.php
+++ resources/views/components/navBar/component-navbar-default-extended.blade.php
@@ -62,20 +62,20 @@ $navBar_ID = ACF_CUSTOM::_getField('nav_global');
                     @if (isset($transparent) && $transparent === true)
                     @if (isset($type) && $type === 'default')
                     <a class="navbar-brand normal" href="{!! home_url() !!}">
-                        <img src="{!! App::setFilePath('/assets/images/logos/logotipo-escala-blanco.png') !!}" alt="Logo" class="logo-img">
+                        <img src="{!! App::setFilePath('/assets/images/logos/log-escala-blanco-2025.webp') !!}" alt="Logo" class="logo-img">
                     </a>
                     @endif
                     @if (isset($type) && $type === 'white')
                     <a class="navbar-brand normal" href="{!! home_url() !!}">
-                        <img src="{!! App::setFilePath('/assets/images/logos/logo-nuevo-escala-azul-oscuro.webp') !!}" alt="Logo" class="logo-img">
+                        <img src="{!! App::setFilePath('/assets/images/logos/log-escala-oscuro-2025.webp') !!}" alt="Logo" class="logo-img">
                     </a>
                     @endif
                     <a class="fixed navbar-brand" href="{!! home_url() !!}">
-                        <img src="{!! App::setFilePath('/assets/images/logos/logo-nuevo-escala-azul-oscuro.webp') !!}" alt="Logo" class="logo-img">
+                        <img src="{!! App::setFilePath('/assets/images/logos/log-escala-oscuro-2025.webp') !!}" alt="Logo" class="logo-img">
                     </a>
                     @else
                     <a class="navbar-brand normal" href="{!! home_url() !!}">
-                        <img src="{!! App::setFilePath('/assets/images/logos/logo-nuevo-escala-azul-oscuro.webp') !!}" alt="Logo" class="logo-img">
+                        <img src="{!! App::setFilePath('/assets/images/logos/log-escala-oscuro-2025.webp') !!}" alt="Logo" class="logo-img">
                     </a>
                     @endif
 
@@ -170,7 +170,7 @@ $navBar_ID = ACF_CUSTOM::_getField('nav_global');
 
             <div class="M_e logo">
                 <a class="navbar-brand" href="{!! home_url() !!}">
-                    <img src="{!! App::setFilePath('/assets/images/logos/logo-nuevo-escala-azul-oscuro.webp') !!}" alt="Logo" class="logo-img">
+                    <img src="{!! App::setFilePath('/assets/images/logos/log-escala-oscuro-2025.webp') !!}" alt="Logo" class="logo-img">
                 </a>
             </div>
 
