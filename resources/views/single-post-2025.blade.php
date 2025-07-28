ca6ae11e417b43679c2bfda0a42ea22e5ae19a38
Ender Brache <enderbrache18@gmail.com>
2025-07-28 17:52:45 -0400

    Ajustando seccion 2 de la plantilla blog

--- resources/views/single-post-2025.blade.php
+++ resources/views/single-post-2025.blade.php
@@ -48,7 +48,7 @@ $blocks = $post['content_blocks'] ?? [];
                     </div>
                     @endif
 
-                    
+
                     <div class="meta meta-mb">
                         <div class="img">
                             <img alt="Icon escala" src="{{ App::setFilePath('/assets/images/icons/icon escala blog.webp') }}" loading="lazy">
@@ -69,37 +69,70 @@ $blocks = $post['content_blocks'] ?? [];
             </div>
 
         </section>
-        <section class="blog-header-section customSection sectionParent fullWidth">
+        <section class="index-section customSection sectionParent fullWidth ">
             <div class="section-row">
-                {{-- Renderizado dinámico de bloques --}}
-                @foreach($blocks as $block)
-                @switch($block['_type'])
-                @case('index')
-                @include('components.blog2025.index', ['index_items' => $block['index_items']])
-                @break
-                @case('text_block')
-                @include('components.blog2025.text-block', ['title' => $block['title'], 'content' => $block['content']])
-                @break
-                @case('highlight_block')
-                @include('components.blog2025.highlight-block', ['content' => $block['content']])
-                @break
-                @case('space_block')
-                @include('components.blog2025.space-block', ['space_size' => $block['space_size']])
-                @break
-                @case('cta_banner')
-                @include('components.blog2025.cta-banner', [
-                'image' => $block['image'],
-                'title' => $block['title'],
-                'content' => $block['content'],
-                'button_text' => $block['button_text'],
-                'button_url' => $block['button_url']
-                ])
-                @break
-                @endswitch
-                @endforeach
+                <div class="innerSectionElement sct0">
+                    {{-- Renderizado dinámico de bloques --}}
+                    @foreach($blocks as $block)
+                    @switch($block['_type'])
+                    @case('index')
+                    @include('components.blog2025.index', ['index_items' => $block['index_items']])
+                    @break
+                    @case('text_block')
+                    @include('components.blog2025.text-block', ['title' => $block['title'], 'content' => $block['content']])
+                    @break
+                    @case('highlight_block')
+                    @include('components.blog2025.highlight-block', ['content' => $block['content']])
+                    @break
+                    @case('space_block')
+                    @include('components.blog2025.space-block', ['space_size' => $block['space_size']])
+                    @break
+                    @case('cta_banner')
+                    @include('components.blog2025.cta-banner', [
+                    'image' => $block['image'],
+                    'title' => $block['title'],
+                    'content' => $block['content'],
+                    'button_text' => $block['button_text'],
+                    'button_url' => $block['button_url']
+                    ])
+                    @break
+                    @endswitch
+                    @endforeach
+                </div>
+                <div class="innerSectionElement sct1">
+                    <section class="share-section">
+                        <h3>Compartir en</h3>
+                        <div class="social-share-buttons">
+                            <a href="https://www.facebook.com/escalasoftware/" target="_blank"> <img src="{{ App::setFilePath('/assets/images/illustrations/others/icon-facebook-escala.webp') }}"></a>
+                            <a href="https://x.com/escalasoftware" target="_blank"> <img src="{{ App::setFilePath('/assets/images/illustrations/others/icon-x-escala.webp') }}"></a>
+                            <a href="https://www.linkedin.com/company/escalacrm/posts/?feedView=all" target="_blank"> <img src="{{ App::setFilePath('/assets/images/illustrations/others/icon-linkedin-escala.webp') }}"></a>
+                        </div>
+                    </section>
+
+                    @php
+                    $recent_posts = new WP_Query([
+                    'post_type' => 'post',
+                    'posts_per_page' => 4,
+                    'post__not_in' => [get_the_ID()],
+                    'orderby' => 'date',
+                    'order' => 'DESC',
+                    ]);
+                    @endphp
+                    <section class="recent-posts-widget">
+                        <h3>Artículos populares</h3>
+                        <ul>
+                            @foreach($recent_posts->posts as $recent)
+                            <li>
+                                <a href="{{ get_permalink($recent->ID) }}">{{ get_the_title($recent->ID) }}</a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </section>
+                    @php wp_reset_postdata(); @endphp
+                </div>
+
             </div>
         </section>
-        {{-- Aquí puedes incluir secciones fijas como compartir o artículos relacionados si lo deseas --}}
 
 
 
