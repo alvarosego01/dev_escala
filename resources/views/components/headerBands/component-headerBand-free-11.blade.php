f63431ea09f1fa9b292f6a80f76caf965cbaeb76
Ender Brache <enderbrache18@gmail.com>
2026-03-06 11:54:51 -0400

    Cambios para onterna seguros

--- resources/views/components/headerBands/component-headerBand-free-11.blade.php
+++ resources/views/components/headerBands/component-headerBand-free-11.blade.php
@@ -1,4 +1,4 @@
-<section style="background-image: url({!! App::setFilePath('/assets/images/banners/bg-mc-oct-2025.webp') !!})" class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth {{ $classSection }} ">
+<section class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth {{ $classSection }} ">
 
     <div class="section-row">
 
@@ -10,17 +10,17 @@
                     <!-- <img src="{!! App::setFilePath('/assets/images/illustrations/others/icon-star-blue.webp') !!}" alt="" class="icon"> -->
 
                     <span class="hashTitle">
-                        Logra que tu EQUIPO VENDA MÁS con WhatsApp automatizado y CRM
-                    </span>
-
+                        Conoce Escala para </span>
+                    <!-- 
                     <div class="separator"></div>
                     <span class="sub">
                         Masterclass gratuita - 15 de octubre
-                    </span>
+                    </span> -->
                 </div>
 
                 <a target="_blank" href="https://experiencia.escala.com/eventos-escala" class=" primaryButton hoverInEffect ">
-                    Reserva tu cupo →
+                    <span>Seguros</span>
+
                 </a>
 
             </div>
@@ -31,30 +31,27 @@
 </section>
 
 
-<section style="background-image: url({!! App::setFilePath('/assets/images/banners/bg-mc-oct-2025-mb.webp') !!})" class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth MbHeadbandfree {{ $classSection }} ">
+<section class="headerband_promo_freeclass_t1 customSection sectionParent fullWidth MbHeadbandfree {{ $classSection }} ">
 
     <div class="section-row">
 
         <section class="innerSectionElement sct1">
             <div class="containElements">
 
-
                 <div class="contain">
                     <!-- <img src="{!! App::setFilePath('/assets/images/illustrations/others/icon-star-blue.webp') !!}" alt="" class="icon"> -->
 
                     <span class="hashTitle">
-                        Logra que tu EQUIPO VENDA MÁS con WhatsApp automatizado y CRM
-                    </span>
-
+                        Conoce Escala para </span>
+                    <!-- 
                     <div class="separator"></div>
                     <span class="sub">
                         Masterclass gratuita - 15 de octubre
-                    </span>
+                    </span> -->
                 </div>
 
-
                 <a target="_blank" href="https://experiencia.escala.com/eventos-escala" class=" primaryButton hoverInEffect ">
-                    Reserva tu cupo →
+                    <span>Seguros</span>
                 </a>
 
             </div>
